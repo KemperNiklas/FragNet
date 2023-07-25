6f9b0b56ec13ed54ef123265e88989313c191141
Niklas Kemper <niklas.kemper@tum.de>
2023-07-25 14:55:06 +0200

    Add model for node classification in single graph

--- first_tests/trainer.py
+++ first_tests/trainer.py
@@ -3,10 +3,12 @@ import torch.nn.functional as F
 import torch
 
 class GraphNet(pl.LightningModule):
-    def __init__(self, model):
+    def __init__(self, model, lr=0.01, weight_decay=5e-4):
         super().__init__()
         self.model = model
         self.metric = F.mse_loss
+        self.lr = lr
+        self.weight_decay = weight_decay
 
     def forward(self, data):
         return self.model(data)
@@ -34,9 +36,46 @@ class GraphNet(pl.LightningModule):
         self.log_dict({f"{prefix}_loss": loss, f"{prefix}_acc": acc})
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.01, weight_decay=5e-4)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay= self.weight_decay)
         return optimizer
 
+class GraphNetNodeClassification(pl.LightningModule):
+    def __init__(self, model, lr=0.01, weight_decay=5e-4):
+        super().__init__()
+        self.model = model
+        self.metric = torch.nn.CrossEntropyLoss()
+        self.lr = lr
+        self.weight_decay = weight_decay
+
+    def forward(self, data):
+        return self.model(data)
+
+    def training_step(self, batch, batch_idx):
+        data = batch
+        x_hat = torch.squeeze(self.model(data))
+        loss = self.metric(x_hat[data.train_mask], data.y[data.train_mask])
+        self.log("training_loss", loss, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        self._shared_eval(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        self._shared_eval(batch, batch_idx, "test")
+
+    def _shared_eval(self, batch, batch_idx, prefix):
+        data = batch
+        mask = data.val_mask if prefix == "val" else data.test_mask
+        x_hat = torch.squeeze(self.model(data))
+        loss = self.metric(x_hat[mask], data.y[mask])
+        correct = torch.argmax(x_hat[mask], dim = 1) == data.y[mask]  # Check against ground-truth labels.
+        acc = int(correct.sum()) / mask.sum()  # Derive ratio of correct predictions.
+        self.log_dict({f"{prefix}_loss": loss, f"{prefix}_acc": acc})
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay= self.weight_decay)
+        return optimizer
+    
 class GraphNetNodePredictions(pl.LightningModule):
     def __init__(self, model, lr=0.01, weight_decay=5e-4):
         super().__init__()
