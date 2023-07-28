f8b3ee8fdd131d5d6db1d7a1a2ea0fe896397abe
Niklas Kemper <niklas.kemper@tum.de>
2023-07-28 15:32:06 +0200

    Add clique discovery method

--- first_tests/substructures.py
+++ first_tests/substructures.py
@@ -6,6 +6,48 @@ import networkx as nx
 import torch
 from itertools import chain
 
+def get_cliques(edge_index: torch.Tensor, max_k: int, min_k: int = 3):
+    """
+    Compute all cliques of given lengths.
+
+    Parameters
+    ----------
+    edge_index
+        Pytorch-geometric style edge index.
+    max_k
+        Maximum number of nodes in ring
+    min_k, optional
+        Minimum number of nodes in ring, by default 3
+
+    Returns
+    -------
+        List of sets of all cliques with length >= min_k and length <= max_k. Permutations of cliques are removed.
+    """
+    if isinstance(edge_index, torch.Tensor):
+        edge_index = edge_index.numpy()
+
+    edge_list = edge_index.T
+    graph_gt = gt.Graph(directed=False)
+    graph_gt.add_edge_list(edge_list)
+    gen.remove_self_loops(graph_gt)
+    gen.remove_parallel_edges(graph_gt)
+    cliques = set()
+    sorted_cliques = set()
+    for k in range(min_k, max_k+1):
+        pattern = nx.complete_graph(k)
+        pattern_edge_list = list(pattern.edges)
+        pattern_gt = gt.Graph(directed=False)
+        pattern_gt.add_edge_list(pattern_edge_list)
+        sub_isos = top.subgraph_isomorphism(pattern_gt, graph_gt, induced=True, subgraph=True,
+                                           generator=True)
+        sub_iso_sets = map(lambda isomorphism: tuple(isomorphism.a), sub_isos)
+        for iso in sub_iso_sets:
+            if tuple(sorted(iso)) not in sorted_cliques:
+                cliques.add(iso)
+                sorted_cliques.add(tuple(sorted(iso)))
+    cliques = list(cliques)
+    return cliques
+
 def get_rings(edge_index: torch.Tensor, max_k: int, min_k: int = 3):
     """
     Compute all rings of given lengths.
@@ -76,4 +118,6 @@ def get_substructure_edge_index(substructure):
     -------
         Pytorch-geometric style edge index from nodes to substructures in which the nodes are part of.
     """
+    if not substructure:
+        return torch.empty(size = (2,0), dtype = torch.long)
     return torch.tensor([[node_id, sub_id]  for sub_id, sub in enumerate(substructure) for node_id in sub], dtype = torch.long).t().contiguous()
