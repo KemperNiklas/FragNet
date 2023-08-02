8d0605b08f9013fc3f01340391f0f9939472161b
Niklas Kemper <niklas.kemper@tum.de>
2023-08-02 11:12:42 +0200

    Fix some parts in the lightning model wrapper

--- models/lightning_models.py
+++ models/lightning_models.py
@@ -2,7 +2,7 @@ import pytorch_lightning as pl
 import torch.nn.functional as F
 import torch
 
-class LightningModel(pl.LighningModule):
+class LightningModel(pl.LightningModule):
     def __init__(self, model, loss, acc, optimizer_parameters):
         super().__init__()
         self.model = model
@@ -17,7 +17,7 @@ class LightningModel(pl.LighningModule):
         data = batch
         x_hat = self.model(data)
         loss = self.loss(torch.squeeze(x_hat), data, "train")
-        self.log("train_loss", loss, on_epoch=False)
+        self.log("train_loss", loss, on_epoch=False, batch_size = _calculate_batch_size(data))
         return loss
     
     def validation_step(self, batch, batch_idx):
@@ -30,13 +30,19 @@ class LightningModel(pl.LighningModule):
         data = batch
         x_hat = self.model(data)
         loss = self.loss(torch.squeeze(x_hat), data, prefix)
-        acc = self.acc(torch.squeeze(x_hat), data, prefix)  
-        self.log_dict({f"{prefix}_loss": loss, f"{prefix}_acc": acc})
+        acc = self.acc(torch.squeeze(x_hat), data, prefix)
+        self.log_dict({f"{prefix}_loss": loss, f"{prefix}_acc": acc}, batch_size = _calculate_batch_size(data))
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), **self.optimizer_parameters)
         return optimizer
 
+def _calculate_batch_size(data):
+    if hasattr(data, "batch"):
+        batch_size = torch.max(data.batch)
+    else:
+        batch_size = 1 
+    return batch_size 
 
 def classification_loss(xhat, data, mode: str):
     metric = torch.nn.CrossEntropyLoss()
@@ -50,9 +56,9 @@ def classification_loss(xhat, data, mode: str):
 def classification_accuracy(xhat, data, mode: str):
     if hasattr(data, f"{mode}_mask"):
         mask = getattr(data, f"{mode}_mask")
-        acc = torch.mean((torch.argmax(xhat[mask], dim = 1) == data.y[mask]).int(), dtype = torch.float32)
+        acc = torch.mean((torch.argmax(xhat[mask], dim = 1) == data.y[mask]).float(), dtype = torch.float32)
     else:
-        acc = torch.mean((torch.argmax(xhat, dim = 1) == data.y).int(), dtype = torch.float32)
+        acc = torch.mean((torch.argmax(xhat, dim = 1) == data.y).float(), dtype = torch.float32)
     return acc
 
 def regression_loss(xhat, data, mode: str):
@@ -67,7 +73,7 @@ def regression_loss(xhat, data, mode: str):
 def regression_acc(xhat, data, mode: str):
     if hasattr(data, f"{mode}_mask"):
         mask = getattr(data, f"{mode}_mask")
-        acc = torch.mean((torch.round(xhat[mask]) == data.y[mask]).int(), dtype=torch.float32) 
+        acc = torch.mean((torch.round(xhat[mask]) == data.y[mask]).float(), dtype=torch.float32) 
     else:
-        acc = torch.mean((torch.round(xhat) == data.y).int(), dtype=torch.float32) 
+        acc = torch.mean((torch.round(xhat) == data.y).float(), dtype=torch.float32) 
     return acc
