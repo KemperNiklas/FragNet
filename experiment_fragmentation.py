a38435a68912f55b3c3baadca8d7d6c23d410151
Tom Wollschlaeger <tom.wollschlaeger@t-online.de>
2024-01-14 00:31:59 +0100

    removes import

--- experiment_fragmentation.py
+++ experiment_fragmentation.py
@@ -3,7 +3,7 @@ import numpy as np
 import seml
 from typing import List, Dict, Optional, Tuple
 import datasets.data
-import datasets.data_ood
+# import datasets.data_ood
 from models.gcn import GCN, VerySimpleGCN, GCNSubstructure, HimpNet, HimpNetHigherGraph, HimpNetAlternative
 from models.hlg import HLG, HLGAlternative, HLG_Old, HLG_HIMP
 from models.substructure_model import SubstructureNeuralNet
@@ -32,7 +32,8 @@ def config():
     overwrite = None
     db_collection = None
     if db_collection is not None:
-        ex.observers.append(seml.create_mongodb_observer(db_collection, overwrite=overwrite))
+        ex.observers.append(
+            seml.create_mongodb_observer(db_collection, overwrite=overwrite))
 
 
 class ExperimentWrapper:
@@ -43,7 +44,19 @@ class ExperimentWrapper:
 
     # With the prefix option we can "filter" the configuration for the sub-dictionary under "data".
     @ex.capture(prefix="data")
-    def init_dataset(self, _config, dataset: str, seed: Optional[int] = None, remove_node_features: bool = False, one_hot_degree: bool = False, one_hot_node_features: bool = False, one_hot_edge_features: bool = False, fragmentation_method: Optional[Tuple[str, str, Dict]] = None, loader_params: Optional[Dict] = None, encoding: List = [], dataset_params = {}):
+    def init_dataset(self,
+                     _config,
+                     dataset: str,
+                     seed: Optional[int] = None,
+                     remove_node_features: bool = False,
+                     one_hot_degree: bool = False,
+                     one_hot_node_features: bool = False,
+                     one_hot_edge_features: bool = False,
+                     fragmentation_method: Optional[Tuple[str, str,
+                                                          Dict]] = None,
+                     loader_params: Optional[Dict] = None,
+                     encoding: List = [],
+                     dataset_params={}):
         """Initialize train, validation and test loader.
 
         Parameters
@@ -71,27 +84,46 @@ class ExperimentWrapper:
         print(f"Dataset received config: {_config}")
         if seed:
             torch.manual_seed(seed)
-        
+
         if fragmentation_method:
             self.num_substructures = fragmentation_method[2]["vocab_size"]
         else:
             self.num_substructures = None
-        
+
         if "filter" in dataset_params:
             # ood experiment
-            self.train_loader, self.val_loader, self.test_loader, self.num_features, self.num_classes = datasets.data_ood.load_fragmentation(dataset, remove_node_features = remove_node_features, one_hot_degree = one_hot_degree,one_hot_node_features=one_hot_node_features,one_hot_edge_features=one_hot_edge_features, fragmentation_method = fragmentation_method , loader_params = loader_params, **dataset_params)
+            self.train_loader, self.val_loader, self.test_loader, self.num_features, self.num_classes = datasets.data_ood.load_fragmentation(
+                dataset,
+                remove_node_features=remove_node_features,
+                one_hot_degree=one_hot_degree,
+                one_hot_node_features=one_hot_node_features,
+                one_hot_edge_features=one_hot_edge_features,
+                fragmentation_method=fragmentation_method,
+                loader_params=loader_params,
+                **dataset_params)
         else:
-            self.train_loader, self.val_loader, self.test_loader, self.num_features, self.num_classes = datasets.data.load_fragmentation(dataset, remove_node_features = remove_node_features, one_hot_degree = one_hot_degree,one_hot_node_features=one_hot_node_features,one_hot_edge_features=one_hot_edge_features, fragmentation_method = fragmentation_method , loader_params = loader_params, encoding = encoding, **dataset_params)
-
-
+            self.train_loader, self.val_loader, self.test_loader, self.num_features, self.num_classes = datasets.data.load_fragmentation(
+                dataset,
+                remove_node_features=remove_node_features,
+                one_hot_degree=one_hot_degree,
+                one_hot_node_features=one_hot_node_features,
+                one_hot_edge_features=one_hot_edge_features,
+                fragmentation_method=fragmentation_method,
+                loader_params=loader_params,
+                encoding=encoding,
+                **dataset_params)
 
     @ex.capture(prefix="model")
-    def init_model(self, model_type: str, model_params: dict, classification: bool = True):
+    def init_model(self,
+                   model_type: str,
+                   model_params: dict,
+                   classification: bool = True):
         self.classification = classification
-        model_params = model_params.copy() # allows us to add fields to it
+        model_params = model_params.copy()  # allows us to add fields to it
         if not "out_channels" in model_params:
             if classification:
-                model_params["out_channels"] = self.num_classes if self.num_classes > 2 else 1
+                model_params[
+                    "out_channels"] = self.num_classes if self.num_classes > 2 else 1
             else:
                 model_params["out_channels"] = self.num_classes
         model_params["in_channels"] = self.num_features
@@ -105,38 +137,45 @@ class ExperimentWrapper:
         elif model_type == "PoolLinear":
             self.model = PoolLinear(**model_params)
         elif model_type == "GlobalLinear":
-            model_params.pop("in_channels") #does not need in_channels
+            model_params.pop("in_channels")  #does not need in_channels
             self.model = GlobalLinear(**model_params)
         elif model_type == "GCNSubstructure":
             model_params["in_channels_substructure"] = self.num_substructures
             self.model = GCNSubstructure(**model_params)
         elif model_type == "HimpNet":
             model_params["in_channels_substructure"] = self.num_substructures
-            model_params["in_channels_edge"] = 4 #TODO: could be different for other datasets
+            model_params[
+                "in_channels_edge"] = 4  #TODO: could be different for other datasets
             self.model = HimpNet(**model_params)
         elif model_type == "HimpNetHigherGraph":
             model_params["in_channels_substructure"] = self.num_substructures
-            model_params["in_channels_edge"] = 4 #TODO: could be different for other datasets
+            model_params[
+                "in_channels_edge"] = 4  #TODO: could be different for other datasets
             self.model = HimpNetHigherGraph(**model_params)
         elif model_type == "HimpNetAlternative":
             model_params["in_channels_substructure"] = self.num_substructures
-            model_params["in_channels_edge"] = 4 #TODO: could be different for other datasets
+            model_params[
+                "in_channels_edge"] = 4  #TODO: could be different for other datasets
             self.model = HimpNetAlternative(**model_params)
         elif model_type == "HLG":
             model_params["in_channels_frag"] = self.num_substructures
-            model_params["in_channels_edge"] = 4 #TODO: could be different for other datasets
+            model_params[
+                "in_channels_edge"] = 4  #TODO: could be different for other datasets
             self.model = HLG(**model_params)
         elif model_type == "HLGAlternative":
             model_params["in_channels_frag"] = self.num_substructures
-            model_params["in_channels_edge"] = 4 #TODO: could be different for other datasets
+            model_params[
+                "in_channels_edge"] = 4  #TODO: could be different for other datasets
             self.model = HLGAlternative(**model_params)
         elif model_type == "HLG_Old":
             model_params["in_channels_frag"] = self.num_substructures
-            model_params["in_channels_edge"] = 4 #TODO: could be different for other datasets
+            model_params[
+                "in_channels_edge"] = 4  #TODO: could be different for other datasets
             self.model = HLG_Old(**model_params)
         elif model_type == "HLG_HIMP":
             model_params["in_channels_frag"] = self.num_substructures
-            model_params["in_channels_edge"] = 4 #TODO: could be different for other datasets
+            model_params[
+                "in_channels_edge"] = 4  #TODO: could be different for other datasets
             self.model = HLG_HIMP(**model_params)
         else:
             raise RuntimeError(f"Model {model_type} not supported")
@@ -144,7 +183,12 @@ class ExperimentWrapper:
         print(self.model)
 
     @ex.capture(prefix="optimization")
-    def init_optimizer(self, optimization_params, scheduler_parameters = None, loss: Optional[str] = None, additional_metric: Optional[str] = None, ema_decay = None):
+    def init_optimizer(self,
+                       optimization_params,
+                       scheduler_parameters=None,
+                       loss: Optional[str] = None,
+                       additional_metric: Optional[str] = None,
+                       ema_decay=None):
         loss_func = None
         if self.classification and self.num_classes > 2:
             loss_func = ce_loss
@@ -158,7 +202,7 @@ class ExperimentWrapper:
             else:
                 loss_func = mse_loss
             acc = regression_acc
-        
+
         additional_metric_func = None
         if additional_metric:
             if additional_metric == "mae":
@@ -170,7 +214,14 @@ class ExperimentWrapper:
             elif additional_metric == "ap":
                 additional_metric_func = average_multilabel_precision
 
-        self.lightning_model = LightningModel(model = self.model, loss = loss_func, acc = acc, optimizer_parameters= optimization_params,scheduler_parameters=scheduler_parameters, additional_metric= additional_metric_func, ema_decay = ema_decay)
+        self.lightning_model = LightningModel(
+            model=self.model,
+            loss=loss_func,
+            acc=acc,
+            optimizer_parameters=optimization_params,
+            scheduler_parameters=scheduler_parameters,
+            additional_metric=additional_metric_func,
+            ema_decay=ema_decay)
 
     def init_all(self):
         """
@@ -181,29 +232,60 @@ class ExperimentWrapper:
         self.init_optimizer()
 
     @ex.capture()
-    def train(self, trainer_params, project_name, _config, notes = ""):
-        
+    def train(self, trainer_params, project_name, _config, notes=""):
+
         checkpoint_directory = f"{checkpoint_dir}/{_config['db_collection']}/run-{_config['overwrite']}"
         if not os.path.exists(checkpoint_directory):
             os.makedirs(checkpoint_directory)
 
-        wandb_logger = WandbLogger(name = f"{_config['db_collection']}_{_config['overwrite']}", project=project_name, save_dir = checkpoint_directory, notes = notes, entity = "frags")
+        wandb_logger = WandbLogger(
+            name=f"{_config['db_collection']}_{_config['overwrite']}",
+            project=project_name,
+            save_dir=checkpoint_directory,
+            notes=notes,
+            entity="frags")
         wandb_logger.experiment.config.update(_config)
         #wandb_logger.watch(self.lightning_model, log="all")
 
         # bar = MeterlessProgressBar() # progress bar without a running bar
 
         if "gradient_clip_val" in trainer_params:
-            additional_params = {"gradient_clip_val": trainer_params["gradient_clip_val"]}
+            additional_params = {
+                "gradient_clip_val": trainer_params["gradient_clip_val"]
+            }
         else:
             additional_params = {}
         if "min_lr" in trainer_params:
-            trainer = Trainer(max_epochs=trainer_params["max_epochs"], logger= wandb_logger, enable_progress_bar= True, log_every_n_steps=15, default_root_dir=f"./models/checkpoints/{_config['db_collection']}-{_config['overwrite']}", detect_anomaly= True, 
-                              callbacks = [EarlyStopping(monitor = "lr", mode = "min", stopping_threshold = trainer_params["min_lr"], check_on_train_epoch_end=True, min_delta=-1)], **additional_params)
+            trainer = Trainer(
+                max_epochs=trainer_params["max_epochs"],
+                logger=wandb_logger,
+                enable_progress_bar=True,
+                log_every_n_steps=15,
+                default_root_dir=
+                f"./models/checkpoints/{_config['db_collection']}-{_config['overwrite']}",
+                detect_anomaly=True,
+                callbacks=[
+                    EarlyStopping(monitor="lr",
+                                  mode="min",
+                                  stopping_threshold=trainer_params["min_lr"],
+                                  check_on_train_epoch_end=True,
+                                  min_delta=-1)
+                ],
+                **additional_params)
         else:
-            trainer = Trainer(max_epochs=trainer_params["max_epochs"], logger= wandb_logger, enable_progress_bar= True, log_every_n_steps=15, default_root_dir=f"./models/checkpoints/{_config['db_collection']}-{_config['overwrite']}", detect_anomaly= True, **additional_params)
-            
-        trainer.fit(self.lightning_model, train_dataloaders=self.train_loader, val_dataloaders= self.val_loader)
+            trainer = Trainer(
+                max_epochs=trainer_params["max_epochs"],
+                logger=wandb_logger,
+                enable_progress_bar=True,
+                log_every_n_steps=15,
+                default_root_dir=
+                f"./models/checkpoints/{_config['db_collection']}-{_config['overwrite']}",
+                detect_anomaly=True,
+                **additional_params)
+
+        trainer.fit(self.lightning_model,
+                    train_dataloaders=self.train_loader,
+                    val_dataloaders=self.val_loader)
         if trainer_params["testing"] == True:
             result = trainer.test(self.lightning_model, self.test_loader)
             wandb.finish()
